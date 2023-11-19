a1ff11db96a5e4d8e2706a8b1368b17579c07361
amitamola <amitamola.dun@gmail.com>
2023-11-19 22:47:47 +0000

    update

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,14 @@
-// use std::io;
+use std::io;
 
 fn main() {
-    // println!("Guess the number!");
+    println!("Guess the number!");
 
-    // println!("Please input your guess.");
+    println!("Please input your guess.");
 
-    // let mut guess = String::new();
+    let mut guess = String::new();
 
-    // io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    // println!("You guessed: {guess}");
-
-    let x = 5;
-    let y = 10;
-
-    println!("x = {} and y + 2 = {}", x, y + 2);
+    println!("You guessed: {guess}");
     
 }
